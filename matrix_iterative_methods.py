ec17807be633cfdc2476dac3b68740220d21f6b9
Ani Mitoyan <animit30@gmail.com>
2025-10-20 18:29:46 +0400

    small changes

--- matrix_iterative_methods.py
+++ matrix_iterative_methods.py
@@ -3,18 +3,17 @@
 # Method 2: x_i = sum(B_ij * x_j) + c_i (Gauss-Seidel method) uses new values as soon as they are available
 
 import numpy as np
-
-b = np.array([[0, (-5/76), (-24/76)], [(-4/66), 0, 2/66], [(-18/46), (-25/46), 0]])
-c = np.array([[19/76], [75/76], [22/46]])
-
+B = np.array([[0, 0.5/7.6, -2.4/7.6], [-0.4/6.6, 0, 0.2/6.6], [-1.8/4.6, -2.5/4.6, 0]])
+C = np.array([[1.9/7.6], [7.5/6.6], [2.2/4.6]])
+eps = 0.0001
 
 x0 = np.array([[0.], [0.], [0.]])
 count_j = 0
 
 while True:
-    x1 = np.dot(b, x0) + c
+    x1 = np.dot(B, x0) + C
     count_j += 1
-    if np.all(np.abs(x1 - x0) < 0.001):
+    if np.all(np.abs(x1 - x0) < eps):
         break
     x0 = x1
 
@@ -27,8 +26,8 @@ while True:
     count_g += 1
     x1 = np.copy(x0)
     for i in range(len(x0)):
-        x0[i] = np.dot(b[i], x0) + c[i]
-    if np.all(np.abs(x1 - x0) < 0.001):
+        x0[i] = np.dot(B[i], x0) + C[i]
+    if np.all(np.abs(x1 - x0) < eps):
         break
 
 print(f"Корень уравнения (Метод 2): {x0.T}, количество итераций: {count_g}")
