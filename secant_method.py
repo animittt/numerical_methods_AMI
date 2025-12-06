bee0847e79e91847bcf560ef518b93061ca1fe72
Ani Mitoyan <animit30@gmail.com>
2025-12-06 16:59:06 +0400

    small changes

--- secant_method.py
+++ secant_method.py
@@ -1,6 +1,8 @@
 #метод секущих  для нахождения корня уравнения f(x) = 0
-def f(x):
-    return x * x * x + x * x - 5
+# x0,x1 - начальные приближения берем такими, что f(x0) и f(x1) имеют разные знаки, и f(x0)f''(x0)>0
+# def f(x):
+#     return x * x * x + x * x - 5
+import math
 
 def secant_method(x0, x1, presicion):
     if abs(f(x0)) < presicion:
@@ -18,8 +20,9 @@ def secant_method(x0, x1, presicion):
 
 
 # пример использования
-x0 = 1.0
-x1 = 2.0
+f = lambda x: math.cos(x) - x
+x0 = 0
+x1 = 1
 presicion = 0.001
 root = secant_method(x0, x1, presicion)
 print(f"Корень уравнения: {root}")
